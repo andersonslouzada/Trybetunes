af83d7a41091607fb87b20562d65dd46ec3ff93b
andersonlouzada <anderson.s.louzada@gmail.com>
2023-03-13 14:51:27 -0300

    Cria componentes de paginas e suas determinadas rotas - Requisito 01

--- src/App.js
+++ src/App.js
@@ -1,8 +1,31 @@
 import React from 'react';
+import { Route, Switch } from 'react-router-dom';
+import Login from './pages/Login';
+import Search from './pages/Search';
+import Album from './pages/Album';
+import Favorite from './pages/Favorite';
+import Profile from './pages/Profile';
+import ProfileEdit from './pages/ProfileEdit';
+import NotFound from './pages/NotFound';
 
 class App extends React.Component {
   render() {
-    return (<p>TrybeTunes!!</p>);
+    return (
+      <main>
+
+        <p>TrybeTunes!!</p>
+        <Switch>
+          <Route exact path="/" component={ Login } />
+          <Route exact path="/search" component={ Search } />
+          <Route exact path="/album/:id" component={ Album } />
+          <Route exact path="/favorites" component={ Favorite } />
+          <Route exact path="/profile" component={ Profile } />
+          <Route path="/profile/edit" component={ ProfileEdit } />
+          <Route path="*" component={ NotFound } />
+        </Switch>
+
+      </main>
+    );
   }
 }
 
---
+++ src/pages/Favorite.js
@@ -0,0 +1,11 @@
+import { Component } from 'react';
+
+class Favorite extends Component {
+  render() {
+    return (
+      <div data-testid="page-favorites">Favoritas: </div>
+    );
+  }
+}
+
+export default Favorite;
