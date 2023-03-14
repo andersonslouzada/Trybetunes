3d46e527821c1544d5d690840864731910b12e06
andersonlouzada <anderson.s.louzada@gmail.com>
2023-03-14 11:32:26 -0300

    cria input e button no component Search conforme requisito 5

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -2,10 +2,38 @@ import { Component } from 'react';
 import Header from '../components/Header';
 
 export default class Search extends Component {
+  state = {
+    searchInput: '',
+    disabledButton: true,
+  };
+
+  handleChange = ({ name, value }) => {
+    const searchMinLength = 2;
+    this.setState({ [name]: value });
+    if (value.length >= searchMinLength) this.setState({ disabledButton: false });
+  };
+
   render() {
+    const { searchInput, disabledButton } = this.state;
     return (
       <div data-testid="page-search">
         <Header />
+        <form>
+          <input
+            name="searchInput"
+            value={ searchInput }
+            onChange={ ({ target }) => this.handleChange(target) }
+            placeholder="Nome do álbum ou artista"
+            data-testid="search-artist-input"
+          />
+          <button
+            type="button"
+            disabled={ disabledButton }
+            data-testid="search-artist-button"
+          >
+            Pesquisar
+          </button>
+        </form>
       </div>
     );
   }
