0947a35cccf37b5b4ac7b5c09549e897cc61fc7c
andersonlouzada <anderson.s.louzada@gmail.com>
2023-03-14 11:21:19 -0300

    cria links no componente Header conforme solicitado no requisito 4

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,26 +1,30 @@
 import { Component } from 'react';
+import { Link } from 'react-router-dom';
 import { getUser } from '../services/userAPI';
 import Loading from '../pages/Loading';
 
 export default class Header extends Component {
   state = {
     isLoading: false,
-    recoveredUser: '',
+    user: '',
   };
 
   async componentDidMount() {
     this.setState({ isLoading: true });
     const user = await getUser();
-    this.setState({ isLoading: false, recoveredUser: user.name });
+    this.setState({ isLoading: false, user: user.name });
   }
 
   render() {
-    const { isLoading, recoveredUser } = this.state;
+    const { isLoading, user } = this.state;
     return (
       <header data-testid="header-component">
-        { !isLoading
-          ? <p data-testid="header-user-name">{recoveredUser}</p>
-          : <Loading />}
+        <nav>
+          <Link to="/search" data-testid="link-to-search">Pesquisar</Link>
+          <Link to="/favorites" data-testid="link-to-favorites">Favoritas</Link>
+          <Link to="/profile" data-testid="link-to-profile">Perfil</Link>
+        </nav>
+        { !isLoading ? <p data-testid="header-user-name">{user}</p> : <Loading />}
       </header>
     );
   }
